b6413e5c0c5445c5f22d33f4f5468cdd5e73710c
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2021-03-12 10:35:52 +0300

    feat: add check() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 != 0) return false;
+
+  let pairsOfBrackets = [];
+
+  for (let value of bracketsConfig)
+    pairsOfBrackets.push(value.join(""));
+
+  for(let j = 0; j < pairsOfBrackets.length; j++){
+    if(str.includes(pairsOfBrackets[j])){
+      str = str.replace(pairsOfBrackets[j], "");
+      j=-1;
+    }
+  }
+  return str.length == 0 ? true : false;
 }
